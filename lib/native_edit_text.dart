cdb05109cbdda12bb11b9ce6f3fa6208213e4b56
Whitiy <59734608+whitiy666@users.noreply.github.com>
2024-08-27 18:59:14 +0800

    first commit

--- lib/native_edit_text.dart
+++ lib/native_edit_text.dart
@@ -1,26 +1,56 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
-class NativeInputWidget extends StatelessWidget {
+typedef TextChangedCallback = void Function(String text);
+typedef TextSubmittedCallback = void Function(String text);
+
+class NativeInputWidget extends StatefulWidget {
+  final TextChangedCallback? onChange;
+  final TextSubmittedCallback? onSubmit;
+  final bool isObscure;
+  final String placeholderText;
+
+  NativeInputWidget({
+    Key? key,
+    this.onChange,
+    this.onSubmit,
+    this.isObscure = false,
+    this.placeholderText = '',
+  }) : super(key: key);
+
+  @override
+  _NativeInputWidgetState createState() => _NativeInputWidgetState();
+}
+
+class _NativeInputWidgetState extends State<NativeInputWidget> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller.addListener(() {
+      if (widget.onChange != null) {
+        widget.onChange!(_controller.text);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AndroidView(
-      viewType: 'native_input_widget_view',
-      onPlatformViewCreated: (int id) {
-        final methodChannel = MethodChannel('native_input_widget_$id');
-        methodChannel.setMethodCallHandler((MethodCall call) async {
-          if (call.method == 'onChange') {
-            print('Text changed: ${call.arguments}');
-          } else if (call.method == 'onSubmit') {
-            print('Text submitted: ${call.arguments}');
-          }
-        });
-      },
-      creationParams: {
-        'placeholderText': 'Enter text',
-        'isObscure': false,
-      },
-      creationParamsCodec: const StandardMessageCodec(),
+    return TextField(
+      controller: _controller,
+      obscureText: widget.isObscure,
+      decoration: InputDecoration(
+        hintText: widget.placeholderText,
+      ),
+      onSubmitted: widget.onSubmit,
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
