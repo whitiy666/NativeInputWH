0818486faa1ea417a369baae46e61ee0350cfc06
Whitiy <59734608+whitiy666@users.noreply.github.com>
2025-03-12 16:30:20 +0800

    Update NativeEditTextPlugin.kt

--- android/src/main/kotlin/com/whitiy/native_edit_text/NativeEditTextPlugin.kt
+++ android/src/main/kotlin/com/whitiy/native_edit_text/NativeEditTextPlugin.kt
@@ -27,8 +27,8 @@ import android.util.Log  // 添加 Log 类的导入
 import android.view.ViewGroup  // 添加 ViewGroup 类的导入
 
 
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RectShape
+//import android.graphics.drawable.ShapeDrawable
+//import android.graphics.drawable.shapes.RectShape
 
 class NativeEditTextPlugin: FlutterPlugin {
     private lateinit var binding: FlutterPlugin.FlutterPluginBinding
